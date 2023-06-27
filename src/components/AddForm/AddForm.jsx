2748b69fc65ae66e994648d0e69c84cb9b427c3b
Szubas <szubas99@gmail.com>
2023-06-27 19:30:54 +0200

    recipes list

--- src/components/AddForm/AddForm.jsx
+++ src/components/AddForm/AddForm.jsx
@@ -9,6 +9,7 @@ export const AddForm = () => {
     const onSubmit = (data) => {
         axios.post('https://recipes-af2a2-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', data)
         .then((response) => {
+            console.log(response);
             navigate("/")
           })
           .catch(function (error) {
--- src/components/AllRecipesList/AllRecipesList.jsx
+++ src/components/AllRecipesList/AllRecipesList.jsx
@@ -1,9 +1,53 @@
 import "./AllRecipesList.css"
+import { useState, useEffect } from "react"
+import axios from 'axios'
 
 export const AllRecipesList = () => {
+    const [recipes, setRecipes] = useState([]);
+    const getRecipes = () => {
+        axios.get('https://recipes-af2a2-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
+        .then((response) => {
+            return response.data;
+          })
+          .then((data) => {
+            const dataRecipes = [];
+    
+            for (const key in data) {
+              const recipe = {
+                id: key,
+                ...data[key]
+              };
+    
+              dataRecipes.push(recipe);
+            }
+            setRecipes(dataRecipes);
+          });
+    }
+
+    
+    
+    useEffect(() => {
+        getRecipes()
+    }, []);
+
     return(
         <>
             <h2>All Recipes List</h2>
+            <div>
+                {
+                    console.log(recipes)
+                }
+                {   
+                    recipes.map((item, id) => (
+                        <div key={id} className="item"> 
+                            <div>{item.title}</div>
+                            <div>{item.type}</div>
+                            <button className="Btn">Details</button>
+                            <button className="Btn">Delete</button>
+                        </div>
+                    ))
+                }
+            </div>  
         </>
     );
 }
