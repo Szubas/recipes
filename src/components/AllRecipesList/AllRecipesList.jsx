f7ee1fb53fbcb66d09e272fbc66e1ee638f4652f
Szubas <szubas99@gmail.com>
2023-06-30 11:54:18 +0200

    search bar

--- src/components/AllRecipesList/AllRecipesList.jsx
+++ src/components/AllRecipesList/AllRecipesList.jsx
@@ -9,6 +9,7 @@ const PER_PAGE = 5;
 export const AllRecipesList = () => {
     const [recipes, setRecipes] = useState([]);
     const [currentPage, setCurrentPage] = useState(0);
+    const [search, setSearch] = useState('');
 
     const getRecipes = () => {
         axios.get('https://recipes-af2a2-default-rtdb.europe-west1.firebasedatabase.app/recipes.json')
@@ -45,44 +46,49 @@ export const AllRecipesList = () => {
     const offset = currentPage * PER_PAGE;
 
     const currentPageData = recipes
+        .filter((item => {
+            return search.toLowerCase() === '' 
+            ? item 
+            : item.title.toLowerCase().includes(search);
+        }))
         .slice(offset, offset + PER_PAGE)
         .map((item, id) => (
-            <>            
+            <>
                 <div key={id} className="item">
-                <div>{item.title}</div>
-                <div>{item.type}</div>
-                <Link to="/details" state={{ title:item.title, type:item.type, igredients:item.ingredients, description:item.description }}> <button className="Btn">Details</button></Link>
-                <Link to="/editrecipe" state={{ id:item.id, title:item.title, type:item.type, igredients:item.ingredients, description:item.description }}> <button className="Btn">Edit</button></Link>
-                <button onClick={() => { delRecipe(item.id) }} className="Btn">Delete</button>
-            </div >
+                    <div>{item.title}</div>
+                    <div>{item.type}</div>
+                    <Link to="/details" state={{ title: item.title, type: item.type, igredients: item.ingredients, description: item.description }}> <button className="Btn">Details</button></Link>
+                    <Link to="/editrecipe" state={{ id: item.id, title: item.title, type: item.type, igredients: item.ingredients, description: item.description }}> <button className="Btn">Edit</button></Link>
+                    <button onClick={() => { delRecipe(item.id) }} className="Btn">Delete</button>
+                </div >
             </>
         ));
 
-const pageCount = Math.ceil(recipes.length / PER_PAGE);
+    const pageCount = Math.ceil(recipes.length / PER_PAGE);
+    useEffect(() => {
+        getRecipes()
+    }, []);
 
-useEffect(() => {
-    getRecipes()
-}, []);
-
-return (
-    <>
-        <h2>All Recipes List</h2>
-        <div>
-            {currentPageData}
-        </div>
-        <ReactPaginate
-            previousLabel={"← Previous"}
-            nextLabel={"Next →"}
-            pageCount={pageCount}
-            pageRangeDisplayed={1}
-            marginPagesDisplayed={1}
-            onPageChange={handlePageClick}
-            containerClassName={"pagination"}
-            previousLinkClassName={"pagination__link"}
-            nextLinkClassName={"pagination__link"}
-            disabledClassName={"pagination__link--disabled"}
-            activeClassName={"pagination__link--active"}
-        />
-    </>
-);
+    return (
+        <>
+            <h2>All Recipes List</h2>
+            <input type='text' placeholder="search by recipe name" onChange={(e) => setSearch(e.target.value)} />
+            <div>
+                {currentPageData}
+            </div>
+            <ReactPaginate
+                previousLabel={"← Previous"}
+                nextLabel={"Next →"}
+                pageCount={pageCount}
+                pageRangeDisplayed={1}
+                marginPagesDisplayed={1}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination"}
+                previousLinkClassName={"pagination__link"}
+                nextLinkClassName={"pagination__link"}
+                disabledClassName={"pagination__link--disabled"}
+                activeClassName={"pagination__link--active"}
+            />
+        </>
+    );
 }
